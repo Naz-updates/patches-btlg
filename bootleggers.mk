95e4cc3d08251c6ff77f0eee0a84674266253dc9
TheAtt1la <alittauwu@gmail.com>
2022-12-09 14:55:15 -0300

    first commit

---
+++ bootleggers.mk
@@ -0,0 +1,13 @@
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+$(call inherit-product, vendor/bootleggers/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/bootleggers/config/BoardConfigbBootleggers.mk)
+$(call inherit-product, device/bootleggers/sepolicy/common/sepolicy.mk)
+-include vendor/bootleggers/build/core/config.mk
+
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+SELINUX_IGNORE_NEVERALLOWS := true
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+
+TARGET_BOOT_ANIMATION_RES := 1080
